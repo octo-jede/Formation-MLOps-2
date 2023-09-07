85e71c12840993d1d48af60c20e0da1fb3678d53
octo-jede <jean-louis.debezia@octo.com>
2023-09-07 08:51:37 +0000

    Versionner les modèles avec un timestamp

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -18,7 +18,7 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     y = features[target]
     model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
     model.fit(X, y)
-    joblib.dump(model, os.path.join(model_registry_folder, 'model.joblib'))
+    joblib.dump(model, os.path.join(model_registry_folder, 'model ' + time.strftime('%Y%m%d-%H%M%S') + '.joblib'))
 
 
 def predict_with_io(features_path: str, model_path: str, predictions_folder: str) -> None:
